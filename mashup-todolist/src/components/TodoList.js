97d8e440c82a0eb2816d19bac1f37fef78382d30
ilyeongJeong <developjiy@gmail.com>
2021-05-19 15:16:37 +0900

    ref #2 Todo List 기능 구성

--- mashup-todolist/src/components/TodoList.js
+++ mashup-todolist/src/components/TodoList.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import TodoItem from './TodoItem';
+
+import { useTodoState } from '../TodoContext';
+
 const TodoListBlock = styled.div`
   flex: 1;
   padding: 20px 32px;
@@ -10,9 +14,18 @@ const TodoListBlock = styled.div`
 `;
 
 function TodoList() {
+  const todos = useTodoState();
+  
   return (
     <TodoListBlock>
-      TodoList
+      {todos.map((todo) => (
+        <TodoItem
+          key={todo.id}
+          id={todo.id}
+          text={todo.text}
+          done={todo.done}     
+        />
+      ))}
     </TodoListBlock>
   );
 }
